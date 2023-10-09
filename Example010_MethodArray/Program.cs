000874e58e0d62dad99e1e4a275bc13e6ccee8f5
Polina <idaya7s@mail.ru>
2023-10-09 16:35:00 +0300

    add Example010_MethodArray/

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 34, 4, 24, 56, 73, 1, 89, 51};
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; // чтобы не выводить номера всех соотвествующих чисел
+    }
+    index ++;
+}
